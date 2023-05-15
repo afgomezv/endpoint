feaecbbaa0e1a07fa46a72d325f12dd3d9930ee4
Andrey Felipe Gomez Villa <andrey.gomez@inder.gov.co>
2023-05-15 14:25:09 -0500

    Se agrega funcionalidad consultar usuarios directorio activo

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
   const ldapClient = getLdapClient();
   //! Hacer algo con el cliente LDAP...
   ldapClient.unbind();
-  res.send("Se conectooooooo!");
+  res.send("Se conecto directorio activo exitoso!");
 });
 
 app.get("/users/:username", (req, res) => {
@@ -20,27 +20,59 @@ app.get("/users/:username", (req, res) => {
     scope: "sub",
   };
 
-  ldapClient.search("DC=inder,DC=gov,DC=co", opts, (err, searchRes) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send("Error en la búsqueda");
+  ldapClient.search(
+    "OU=Usuarios,OU=INDER,DC=inder,DC=gov,DC=local",
+    opts,
+    (err, searchRes) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send("Error en la búsqueda: " + err.message);
+      }
+
+      let users = [];
+      let hasResults = false; // Variable de bandera
+
+      searchRes.on("searchEntry", (entry) => {
+        //* Para cada entrada que coincida con el filtro,
+        //* se ejecutará esta función
+        users.push(entry.pojo);
+        hasResults = true; // Se establece la bandera en verdadero
+        //console.log("Entrada encontrada:", entry.json);
+      });
+
+      searchRes.on("searchReference", (referral) => {
+        //* Si se reciben referencias de búsqueda, se puede
+        //* agregar una lógica adicional aquí si es necesario
+        console.log("Referencia de búsqueda: " + referral.uris.join());
+      });
+
+      searchRes.on("error", (err) => {
+        //* Si ocurre un error durante la búsqueda,
+        //* se ejecutará esta función
+        console.error(err);
+        res.status(500).send("Error en la búsqueda: " + err.message);
+      });
+
+      searchRes.on("end", (result) => {
+        //* Cuando se haya completado la búsqueda, se ejecutará
+        //* esta función con todos los resultados encontrados
+        if (result.status === 0) {
+          ldapClient.unbind();
+
+          if (hasResults) {
+            res.send(users);
+          } else {
+            res.status(404).send("Usuario no encontrado");
+          }
+        } else {
+          const errorMessage =
+            "Búsqueda incompleta. Código de estado: " + result.status;
+          console.error(errorMessage);
+          res.status(500).send("Error en la búsqueda: " + errorMessage);
+        }
+      });
     }
-
-    let users = [];
-
-    searchRes.on("searchEntry", (entry) => {
-      // Para cada entrada que coincida con el filtro,
-      // se ejecutará esta función
-      users.push(entry.object);
-    });
-
-    searchRes.on("end", () => {
-      // Cuando se haya completado la búsqueda, se ejecutará
-      // esta función con todos los resultados encontrados
-      ldapClient.unbind();
-      res.send(users);
-    });
-  });
+  );
 });
 
 app.listen(4000, () => {
