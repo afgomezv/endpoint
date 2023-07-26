ff5a9b0a869de01c7da413320aa63304561fda30
Andrey Felipe Gomez Villa <andrey.gomez@inder.gov.co>
2023-07-26 10:11:50 -0500

    se agrega endpoint de autenticacion y buscador combinados

--- index.js
+++ index.js
@@ -15,67 +15,67 @@ app.get("/", (req, res) => {
   res.send("Se conecto directorio activo exitoso!");
 });
 
-//* Endpoint: buscador de usuarios del directorio activo
-app.get("/:username", (req, res) => {
-  const username = req.params.username;
+// //* Endpoint: buscador de usuarios del directorio activo
+// app.get("/:username", (req, res) => {
+//   const username = req.params.username;
 
-  const ldapClient = getLdapClient();
+//   const ldapClient = getLdapClient();
 
-  const opts = {
-    filter: `(samAccountName=${username})`,
-    scope: "sub",
-  };
+//   const opts = {
+//     filter: `(samAccountName=${username})`,
+//     scope: "sub",
+//   };
 
-  ldapClient.search(
-    "OU=Usuarios,OU=INDER,DC=inder,DC=gov,DC=local",
-    opts,
-    (err, searchRes) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).send("Error en la búsqueda: " + err.message);
-      }
+//   ldapClient.search(
+//     "OU=Usuarios,OU=INDER,DC=inder,DC=gov,DC=local",
+//     opts,
+//     (err, searchRes) => {
+//       if (err) {
+//         console.error(err);
+//         return res.status(500).send("Error en la búsqueda: " + err.message);
+//       }
 
-      let users = [];
-      let hasResults = false; //? Variable de bandera
+//       let users = [];
+//       let hasResults = false; //? Variable de bandera
 
-      //* Para cada entrada que coincida con el filtro, se ejecutará esta función
-      searchRes.on("searchEntry", (entry) => {
-        users.push(entry.pojo.attributes);
-        hasResults = true; //? Se establece la bandera en verdadero
-        //console.log("Entrada encontrada:", entry.pojo.attributes);
-      });
+//       //* Para cada entrada que coincida con el filtro, se ejecutará esta función
+//       searchRes.on("searchEntry", (entry) => {
+//         users.push(entry.pojo.attributes);
+//         hasResults = true; //? Se establece la bandera en verdadero
+//         //console.log("Entrada encontrada:", entry.pojo.attributes);
+//       });
 
-      //* Si se reciben referencias de búsqueda, se puede agregar una lógica adicional aquí si es necesario
-      searchRes.on("searchReference", (referral) => {
-        console.log("Referencia de búsqueda: " + referral.uris.join());
-      });
+//       //* Si se reciben referencias de búsqueda, se puede agregar una lógica adicional aquí si es necesario
+//       searchRes.on("searchReference", (referral) => {
+//         console.log("Referencia de búsqueda: " + referral.uris.join());
+//       });
 
-      //* Si ocurre un error durante la búsqueda, se ejecutará esta función
-      searchRes.on("error", (err) => {
-        console.error(err);
-        res.status(500).send("Error en la búsqueda: " + err.message);
-      });
+//       //* Si ocurre un error durante la búsqueda, se ejecutará esta función
+//       searchRes.on("error", (err) => {
+//         console.error(err);
+//         res.status(500).send("Error en la búsqueda: " + err.message);
+//       });
 
-      //* Cuando se haya completado la búsqueda, se ejecutará esta función con todos los resultados encontrados
-      searchRes.on("end", (result) => {
-        if (result.status === 0) {
-          ldapClient.unbind();
+//       //* Cuando se haya completado la búsqueda, se ejecutará esta función con todos los resultados encontrados
+//       searchRes.on("end", (result) => {
+//         if (result.status === 0) {
+//           ldapClient.unbind();
 
-          if (hasResults) {
-            res.send(users);
-          } else {
-            res.status(404).send("Usuario no encontrado");
-          }
-        } else {
-          const errorMessage =
-            "Búsqueda incompleta. Código de estado: " + result.status;
-          console.error(errorMessage);
-          res.status(500).send("Error en la búsqueda: " + errorMessage);
-        }
-      });
-    }
-  );
-});
+//           if (hasResults) {
+//             res.send(users);
+//           } else {
+//             res.status(404).send("Usuario no encontrado");
+//           }
+//         } else {
+//           const errorMessage =
+//             "Búsqueda incompleta. Código de estado: " + result.status;
+//           console.error(errorMessage);
+//           res.status(500).send("Error en la búsqueda: " + errorMessage);
+//         }
+//       });
+//     }
+//   );
+// });
 
 //*Endpoint: Autenticar usuarios del directorio activo
 app.post("/auth", (req, res) => {
@@ -111,6 +111,61 @@ app.post("/auth", (req, res) => {
             } else {
               //* La autenticación ha sido exitosa
               res.send("Autenticación exitosa");
+            }
+          });
+        });
+
+        //* Error en la búsqueda del usuario
+        searchRes.on("error", (error) => {
+          res.status(500).send("Error en la búsqueda del usuario");
+        });
+
+        //* No se encontró ningún usuario en el directorio activo
+        searchRes.on("end", () => {
+          if (!userFound) {
+            res.status(404).send("Usuario no encontrado");
+          }
+        });
+      }
+    }
+  );
+});
+
+//! Endpoint para autenticar usuarios y buscar en el directorio activo (método POST)
+app.post("/auth/:username", (req, res) => {
+  const username = req.params.username;
+  const password = req.body.password;
+  const ldapClient = getLdapClient();
+
+  const opts = {
+    filter: `(samAccountName=${username})`,
+    scope: "sub",
+  };
+
+  ldapClient.search(
+    "OU=Usuarios,OU=INDER,DC=inder,DC=gov,DC=local",
+    opts,
+    (err, searchRes) => {
+      if (err) {
+        //* Error al realizar la búsqueda
+        res.status(500).send("Error en la búsqueda del usuario");
+      } else {
+        let userFound = false;
+
+        searchRes.on("searchEntry", (entry) => {
+          //* Se encontró un usuario en el directorio activo
+          userFound = true;
+          const userDn = entry.dn.toString();
+
+          //* Autenticar al usuario utilizando su contraseña
+          ldapClient.bind(userDn, password, (bindErr) => {
+            if (bindErr) {
+              //* La autenticación ha fallado
+              res.status(401).send("Credenciales inválidas");
+            } else {
+              //* La autenticación ha sido exitosa
+              const userData = entry.pojo.attributes;
+              res.send(userData);
             }
           });
         });
