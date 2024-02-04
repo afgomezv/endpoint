c5481087c96761ea4f0db819968a2e42c985173a
Andrey Gomez <afgomezvonline@gmail.com>
2024-02-04 10:06:03 -0500

    se agrega endpoint trae todos usuarios AD

--- index.js
+++ index.js
@@ -10,74 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 //! Endpoint: verifica la conexion al directorio activo
 app.get("/", (req, res) => {
   const ldapClient = getLdapClient();
-  //! Hacer algo con el cliente LDAP...
   ldapClient.unbind();
   res.send("Se conecto directorio activo exitoso!");
 });
 
-// //* Endpoint: buscador de usuarios del directorio activo
-// app.get("/:username", (req, res) => {
-//   const username = req.params.username;
-
-//   const ldapClient = getLdapClient();
-
-//   const opts = {
-//     filter: `(samAccountName=${username})`,
-//     scope: "sub",
-//   };
-
-//   ldapClient.search(
-//     "OU=Usuarios,OU=INDER,DC=inder,DC=gov,DC=local",
-//     opts,
-//     (err, searchRes) => {
-//       if (err) {
-//         console.error(err);
-//         return res.status(500).send("Error en la búsqueda: " + err.message);
-//       }
-
-//       let users = [];
-//       let hasResults = false; //? Variable de bandera
-
-//       //* Para cada entrada que coincida con el filtro, se ejecutará esta función
-//       searchRes.on("searchEntry", (entry) => {
-//         users.push(entry.pojo.attributes);
-//         hasResults = true; //? Se establece la bandera en verdadero
-//         //console.log("Entrada encontrada:", entry.pojo.attributes);
-//       });
-
-//       //* Si se reciben referencias de búsqueda, se puede agregar una lógica adicional aquí si es necesario
-//       searchRes.on("searchReference", (referral) => {
-//         console.log("Referencia de búsqueda: " + referral.uris.join());
-//       });
-
-//       //* Si ocurre un error durante la búsqueda, se ejecutará esta función
-//       searchRes.on("error", (err) => {
-//         console.error(err);
-//         res.status(500).send("Error en la búsqueda: " + err.message);
-//       });
-
-//       //* Cuando se haya completado la búsqueda, se ejecutará esta función con todos los resultados encontrados
-//       searchRes.on("end", (result) => {
-//         if (result.status === 0) {
-//           ldapClient.unbind();
-
-//           if (hasResults) {
-//             res.send(users);
-//           } else {
-//             res.status(404).send("Usuario no encontrado");
-//           }
-//         } else {
-//           const errorMessage =
-//             "Búsqueda incompleta. Código de estado: " + result.status;
-//           console.error(errorMessage);
-//           res.status(500).send("Error en la búsqueda: " + errorMessage);
-//         }
-//       });
-//     }
-//   );
-// });
-
-//!Endpoint: Autenticar usuarios del directorio activo
+//!Endpoint: Autenticar usuarios del directorio activo (método POST)
 app.post("/auth", (req, res) => {
   const { username, password } = req.body;
   const ldapClient = getLdapClient();
@@ -101,7 +38,6 @@ app.post("/auth", (req, res) => {
           //* Se encontró un usuario en el directorio activo
           userFound = true;
           const userDn = entry.dn.toString();
-          //console.log(userDn);
 
           //* Autenticar al usuario utilizando su contraseña
           ldapClient.bind(userDn, password, (bindErr) => {
@@ -131,7 +67,7 @@ app.post("/auth", (req, res) => {
   );
 });
 
-//! Endpoint para autenticar usuarios y buscar en el directorio activo (método POST)
+//! Endpoint para autenticar usuarios y traer todos sus valores (método POST)
 app.post("/auth/:username", (req, res) => {
   const username = req.params.username;
   const password = req.body.password;
@@ -166,6 +102,127 @@ app.post("/auth/:username", (req, res) => {
               //* La autenticación ha sido exitosa
               const userData = entry.pojo.attributes;
               res.send(userData);
+            }
+          });
+        });
+
+        //* Error en la búsqueda del usuario
+        searchRes.on("error", (error) => {
+          res.status(500).send("Error en la búsqueda del usuario");
+        });
+
+        //* No se encontró ningún usuario en el directorio activo
+        searchRes.on("end", () => {
+          if (!userFound) {
+            res.status(404).send("Usuario no encontrado");
+          }
+        });
+      }
+    }
+  );
+});
+
+//! Obtener todos los usuarios del directorio activo con autenticación (POST)
+app.post("/users", (req, res) => {
+  const { username, password } = req.body;
+  const ldapClient = getLdapClient();
+
+  const opts = {
+    filter: `(samAccountName=${username})`,
+    scope: "sub",
+  };
+
+  ldapClient.search(
+    "OU=Usuarios,OU=INDER,DC=inder,DC=gov,DC=local",
+    opts,
+    (err, searchRes) => {
+      if (err) {
+        //* Error al realizar la búsqueda
+        res.status(500).send("Error en la búsqueda del usuario");
+      } else {
+        let userFound = false;
+
+        searchRes.on("searchEntry", (entry) => {
+          //* Se encontró un usuario en el directorio activo
+          userFound = true;
+          const userDn = entry.dn.toString();
+
+          //* Autenticar al usuario utilizando su contraseña
+          ldapClient.bind(userDn, password, (bindErr) => {
+            if (bindErr) {
+              //* La autenticación ha fallado
+              res.status(401).send("Credenciales inválidas");
+            } else {
+              //* La autenticación ha sido exitosa
+              const optsAll = {
+                filter: "(objectClass=user)",
+                scope: "sub",
+                attributes: [
+                  "employeeID",
+                  "givenName",
+                  "sn",
+                  "mail",
+                  "userParameters",
+                  "employeeType",
+                ],
+              };
+
+              ldapClient.search(
+                "OU=Usuarios,OU=INDER,DC=inder,DC=gov,DC=local",
+                optsAll,
+                (err, searchRes) => {
+                  if (err) {
+                    console.error("Error en la búsqueda de usuarios:", err);
+                    res.status(500).send("Error en la búsqueda de usuarios");
+                    return;
+                  }
+
+                  const usuarios = [];
+
+                  searchRes.on("searchEntry", (entry) => {
+                    const userData = {};
+
+                    // Iterar sobre los atributos y agregarlos al objeto userData
+                    entry.attributes.forEach((attribute) => {
+                      // Verificar si el atributo tiene valores antes de acceder a ellos
+                      if (attribute.vals && attribute.vals.length > 0) {
+                        // Separar givenName en primerNombre y segundoNombre
+                        if (attribute.type === "givenName") {
+                          const [firstName, secondName] =
+                            attribute.vals[0].split(" ");
+                          userData["firstName"] = firstName;
+                          userData["secondName"] = secondName || "";
+                        }
+                        // Separar sn en primerApellido y segundoApellido
+                        else if (attribute.type === "sn") {
+                          const [surname, secondSurname] =
+                            attribute.vals[0].split(" ");
+                          userData["surname"] = surname;
+                          userData["secondSurname"] = secondSurname || "";
+                        }
+                        // Otros atributos se agregan directamente
+                        else {
+                          userData[attribute.type] = attribute.vals[0];
+                        }
+                      }
+                    });
+
+                    usuarios.push(userData);
+                  });
+
+                  searchRes.on("error", (error) => {
+                    console.error("Error en la búsqueda de usuarios:", error);
+                    res.status(500).send("Error en la búsqueda de usuarios");
+                  });
+
+                  searchRes.on("end", () => {
+                    // Cerrar el cliente LDAP al finalizar la búsqueda
+                    ldapClient.unbind();
+
+                    res.json(usuarios);
+                  });
+                }
+              );
             }
           });
         });
